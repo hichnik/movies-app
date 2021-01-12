00281e270f1defdf03cc837bab1a679c315509d0
Kateryna Porshnieva <k.porshnieva@gmail.com>
2021-01-12 10:56:47 +0200

    Display a list of movies

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,19 +1,25 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 function HomePage() {
+  const [data, setData] = useState(null);
+
   useEffect(() => {
     async function fetchData() {
-        const response = await fetch('https://api.themoviedb.org/3/movie/550?api_key=8c9f0b25d628dc3a96bcc112d2c82e63');
+        const response = await fetch('https://api.themoviedb.org/3/trending/movie/week?api_key=8c9f0b25d628dc3a96bcc112d2c82e63');
         const json = await response.json();
-        console.log(json);
+        setData(json);
     }
 
     fetchData();
   }, []);
 
+  if (!data) {
+      return <div>Loading...</div>;
+  }
+
   return (
     <div>
-      Hello
+      {data.results.map(movie => movie.title)}
     </div>
   );
 }
