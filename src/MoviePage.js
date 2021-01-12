15c201f991f430bc9bf87674ce179319ae83d88d
Kateryna Porshnieva <k.porshnieva@gmail.com>
2021-01-12 12:13:13 +0200

    Add empty MoviePage component

---
+++ src/MoviePage.js
@@ -0,0 +1,11 @@
+import styles from "./MoviePage.module.css";
+
+function MoviePage() {
+  return (
+    <div className={styles.root}>
+      <h2>Movie</h2>
+    </div>
+  );
+}
+
+export default MoviePage;
