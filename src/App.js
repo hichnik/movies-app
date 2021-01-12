b20771003f94b0946975c658f4dd79cdfc466bb9
Kateryna Porshnieva <k.porshnieva@gmail.com>
2021-01-12 12:10:13 +0200

    Add react-router

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import "@veriff/genoma/lib/base.css";
 import { AppHeader } from "@veriff/genoma";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import HomePage from "./HomePage";
 
 function App() {
   return (
-    <div>
+    <Router>
       <AppHeader>
         <AppHeader.Title>Movie app</AppHeader.Title>
       </AppHeader>
-      <HomePage />
-    </div>
+      <Switch>
+        <Route path="/about">About</Route>
+        <Route path="/" component={HomePage} />
+      </Switch>
+    </Router>
   );
 }
 
